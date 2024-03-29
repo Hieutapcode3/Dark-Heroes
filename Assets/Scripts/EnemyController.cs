2b9d699054846d6036ce435a569fff9d4f99f204
Hieutapcode3 <hieupham9155@gmail.com>
2024-03-29 17:11:24 +0700

    update player&enemy movement

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,10 +1,12 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] private float Speed = 1.5f;
+    [SerializeField] private float CurrentTime ;
+    [SerializeField] private float RangeTime = 2f ;
     protected Rigidbody2D rb;
     protected SpriteRenderer spriteRenderer;
     protected Animator anim;
@@ -24,25 +26,48 @@ public class EnemyController : MonoBehaviour
         Movement();
         
     }
-     protected void Movement()
+    public void setSpeed(float speed)
     {
+        this.Speed = speed;
+    }
+    public float getSpeed()
+    {
+        return Speed;
+    }
+    protected void Movement()
+    {
+        anim.SetInteger("State", 2);
         Vector3 target = transforms[currentposition].position;
         transform.position = Vector3.MoveTowards(transform.position, target, Speed * Time.deltaTime);
+
+        // Kiểm tra nếu đã đến điểm đích
         if (transform.position == transforms[currentposition].position)
         {
-            currentposition++;
-            if (currentposition >= transforms.Length)
+            // Dừng animation và kiểm tra thời gian
+            anim.SetInteger("State", 0);
+            setSpeed(0);
+            CurrentTime += Time.deltaTime;
+            if (CurrentTime >= RangeTime)
             {
-                currentposition = 0;
-            }
-            if (currentposition == 1)
-            {
-                spriteRenderer.flipX = true;
-            }
-            else
-            {
-                spriteRenderer.flipX = false;
+                anim.SetInteger("State", 2);
+                setSpeed(1.5f);
+                CurrentTime = 0f;
+                currentposition++;
+                if (currentposition >= transforms.Length)
+                {
+                    currentposition = 0;
+                }
+                if (currentposition == 1)
+                {
+                    spriteRenderer.flipX = true;
+                }
+                else
+                {
+                    spriteRenderer.flipX = false;
+                }
             }
         }
     }
 }
+
+
